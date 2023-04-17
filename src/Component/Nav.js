7c4865d427cdea44220df506dd0113f97552f093
Arun <yarunkumar136@gmail.com>
2023-04-17 14:16:39 +0530

    ass-05

---
+++ src/Component/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <>
+            <div className="nav-Container">
+
+                <div className="nav-body">
+                    <div className="logo">
+                        <h1>Logo</h1>
+                    </div>
+                    <div className="list">
+                        <div className="list1">
+                            <li>Home</li>
+
+                        </div>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Contact</li>
+                    </div>
+
+                </div>
+                <div className="footer">
+                    <h1> Made by me</h1>
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Nav
